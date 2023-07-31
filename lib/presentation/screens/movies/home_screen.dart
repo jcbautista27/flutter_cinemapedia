44164ac99d8fb5a3ce4249538d15225408a86a67
Juan Carlos Bautista <jcbautista2701@gmail.com>
2023-07-30 19:17:03 -0500

    environment variables completed

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const name = 'home-screen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(Environment.theMovieBdKey),
+      ),
+    );
+  }
+}
